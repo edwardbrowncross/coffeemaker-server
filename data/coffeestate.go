7a5aa1322f933b93d82c58102d11b8c1d2d54fb9
Edward <edward.browncross@infinityworks.com>
2018-08-07 10:33:08 +0100

    update json structure

--- data/coffeestate.go
+++ data/coffeestate.go
@@ -4,7 +4,10 @@ import "time"
 
 // CoffeeState holds the parameters sent by the coffee machine to report its state.
 type CoffeeState struct {
-	Heating      bool      `json:"heating"`
+	Heating      bool      `json:"heaterOn"`
+	JugPresent   bool      `json:"jugPresent"`
+	LightState   string    `json:"light"`
 	CoffeeState  string    `json:"coffee"`
+	CoffeeWeight int64     `json:"coffeeWeight"`
 	LastBrewTime time.Time `json:"lastBrewTime"`
 }
