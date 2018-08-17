22c3dee54aed1509780d9f4473751368f4851e02
Edward <edward.browncross@infinityworks.com>
2018-08-17 08:39:59 +0100

    Adjust weight of one portion of coffee

--- data/coffeestate.go
+++ data/coffeestate.go
@@ -2,6 +2,8 @@ package data
 
 import "time"
 
+const coffeePortion = 200
+
 // CoffeeState holds the parameters sent by the coffee machine to report its state.
 type CoffeeState struct {
 	Heating      bool      `json:"heaterOn"`
@@ -12,10 +14,12 @@ type CoffeeState struct {
 	LastBrewTime time.Time `json:"lastBrewTime"`
 }
 
+// GetCupsRemaining returns the number of cups left in the jug
 func (c *CoffeeState) GetCupsRemaining() int {
-	return int((c.CoffeeWeight + 125) / 250.0)
+	return int((c.CoffeeWeight + coffeePortion/2) / coffeePortion)
 }
 
+// GetTimeSinceLastBrew returns the duration since the coffee machine was last tuned on.
 func (c *CoffeeState) GetTimeSinceLastBrew() time.Duration {
 	return time.Now().Sub(c.LastBrewTime)
 }
